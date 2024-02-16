9f630eb1ac9f4ae907d08f6cc9de18e6e4e2c3d9
RaviTeja9399 <159585770+RaviTeja9399@users.noreply.github.com>
2024-02-15 20:15:13 -0500

    Delete Integration directory

--- Integration/Integration_test.py
+++
@@ -1,8 +0,0 @@
-import larger_number
-
-def test_c_larger_number():
- assert larger_number == num1 or larger_number == num2, "Incorrect result"
-if num1 > num2:
-    assert larger_number == num1, "Incorrect larger number"
-else:
-    assert larger_number == num2, "Incorrect larger number"
--- Integration/Larger_number.py
+++
@@ -1,5 +0,0 @@
-def Larger_number(num1, num2):
-    if num1 > num2:
-        return num1
-    else:
-        return num2
