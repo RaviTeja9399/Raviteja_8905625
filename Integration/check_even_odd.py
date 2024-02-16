c190f8e5b24a95d45385fc74f595de7a24ec2b8b
RaviTeja9399 <159585770+RaviTeja9399@users.noreply.github.com>
2024-02-15 20:21:13 -0500

    Update and rename check_even_odd to check_even_odd.py

---
+++ Integration/check_even_odd.py
@@ -0,0 +1,8 @@
+
+
+
+
+assert check_even_odd(4) == "Even", "Error: 4 should be Even"
+assert check_even_odd(7) == "Odd", "Error: 7 should be Odd"
+assert check_even_odd(0) == "Even", "Error: 0 should be Even"
+assert check_even_odd(-1) == "Odd", "Error: -1 should be Odd"
