08635027d19221b869a6f94c64f7d6cf6f868326
RaviTeja9399 <159585770+RaviTeja9399@users.noreply.github.com>
2024-02-15 19:52:10 -0500

    Update Integration_test.py

--- Integration/Integration_test.py
+++ Integration/Integration_test.py
@@ -1,5 +1,7 @@
-# Assert statements to validate the result
-assert larger_number == num1 or larger_number == num2, "Incorrect result"
+import larger_number
+
+def test_c_larger_number():
+ assert larger_number == num1 or larger_number == num2, "Incorrect result"
 if num1 > num2:
     assert larger_number == num1, "Incorrect larger number"
 else:
