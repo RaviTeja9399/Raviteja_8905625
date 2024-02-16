82a689828005a4e7a991dc8e318bfe4a9107ccea
RaviTeja9399 <159585770+RaviTeja9399@users.noreply.github.com>
2024-02-16 11:18:05 -0500

    Delete Integration directory

--- Integration/check_even_odd.py
+++
@@ -1,8 +0,0 @@
-
-
-
-
-assert check_even_odd(4) == "Even", "Error: 4 should be Even"
-assert check_even_odd(7) == "Odd", "Error: 7 should be Odd"
-assert check_even_odd(0) == "Even", "Error: 0 should be Even"
-assert check_even_odd(-1) == "Odd", "Error: -1 should be Odd"
